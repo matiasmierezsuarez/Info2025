1895881045dac1c4e13be745d03ecd6f80271775
matiasmierezsuarez <matiassamu5@gmail.com>
2025-12-06 17:08:45 -0300

    Se agregan vistas y templates para editar y eliminar comentarios, restringir la edicion y eliminacion de post solo para el superuser

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, ComentarioDeleteView, ComentarioUpdateView
 
 app_name = 'apps.posts'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('crear/', PostCreateView.as_view(), name='post_crear'),
     path('editar/<int:pk>/', PostUpdateView.as_view(), name='post_editar'),
     path('eliminar/<int:pk>/', PostDeleteView.as_view(), name='post_eliminar'),
+    path('comentario/eliminar/<int:pk>/', ComentarioDeleteView.as_view(), name='comentario_eliminar'),
+    path('comentario/editar/<int:pk>/', ComentarioUpdateView.as_view(), name='comentario_editar'),
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin   # <-- añadido UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post, Categoria, Comentario
 from .forms import PostForm, ComentarioForm
+
 
 # 1. Listado de Posts (Con filtros)
 class PostListView(ListView):
@@ -56,26 +57,73 @@ class PostDetailView(DetailView):
             comentario.save()
             return redirect('apps.posts:post_individual', pk=self.object.pk)
         return self.render_to_response(self.get_context_data(form=form))
+    
+    def abm_comentario(self, request):
+        comentario = self.get_object()
+        return comentario.usuario == self.request.user
 
 # 3. Vistas CRUD (Solo usuarios logueados)
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView(LoginRequiredMixin,UserPassesTestMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'posts/post_form.html'
     success_url = reverse_lazy('apps.posts:posts')
 
+
+    def test_func(self):
+        return self.request.user.is_superuser
+    
     def form_valid(self, form):
         form.instance.autor = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+    def handle_no_permission(self):
+        return redirect('apps.posts:posts')
+
+# Protegemos actualización para que solo el autor pueda editar
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'posts/post_form.html'
     success_url = reverse_lazy('apps.posts:posts')
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return post.autor == self.request.user
+
+
+
+# Protegemos eliminación para que solo el autor pueda borrar
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'posts/post_confirm_delete.html'
     success_url = reverse_lazy('apps.posts:posts')
 
+    def test_func(self):
+        post = self.get_object()
+        return post.autor == self.request.user
+
+class ComentarioUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comentario
+    form_class = ComentarioForm
+    template_name = 'comentarios/comentario_editar.html' # Crea este template sencillo
+
+    def get_success_url(self):
+        # Redirige de nuevo al detalle del post al que pertenece el comentario
+        return reverse_lazy('apps.posts:post_individual', kwargs={'pk': self.object.post.pk})
+
+    def test_func(self):
+        # Esta función verifica si el usuario logueado es el dueño del comentario
+        comentario = self.get_object()
+        return self.request.user == comentario.usuario
+
+class ComentarioDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comentario
+    template_name = 'comentarios/comentario_eliminar.html' # Template de confirmación
+
+    def get_success_url(self):
+        return reverse_lazy('apps.posts:post_individual', kwargs={'pk': self.object.post.pk})
+
+    def test_func(self):
+        comentario = self.get_object()
+        return self.request.user == comentario.usuario
