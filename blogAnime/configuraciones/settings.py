dd657bba2bc30e79fbed236611514fcefea2df7d
Matias Mierez Suarez <69938548+matiasmierezsuarez@users.noreply.github.com>
2025-12-12 22:24:21 -0300

    Update settings.py
    Se modifica la configuracion para subir a produccion

--- blogAnime/configuraciones/settings.py
+++ blogAnime/configuraciones/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2wxs^0(v(n+wcq&qw4gd)h=v)qh#b80atoqm^+u4ag^q^gfpbv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
